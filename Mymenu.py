8c7a57022eaacb0ffe24e5b92f7689f8b686203d
afeucht <andrew.feucht4@gmail.com>
2021-10-03 15:01:36 -0700

    attempt at grid - bugged

--- Mymenu.py
+++ Mymenu.py
@@ -1,6 +1,8 @@
 import tkinter
 from tkinter import *
 from tkinter import messagebox
+from array import *
+import math
 
 
 class Mymenu:
@@ -19,37 +21,55 @@ class Mymenu:
         gameFrame.place(anchor=NW)
         gameFrame.tkraise()
 
+        gridFrame=Frame(window,width=screenWidth/4, height=screenHeight/2)
+        rowNumber=5
+        colNumber=5
+        colorfield=[[""]*colNumber]*rowNumber
+
+        for x in range(rowNumber):
+            for y in range(colNumber):
+                colorfield[x][y]="gray"
+
+        colorfield[math.floor(rowNumber/2)][math.floor(colNumber/2)]="red"
+
+        for x in range(rowNumber):
+            for y in range(colNumber):
+                square=tkinter.Label(gridFrame, text = "    ",bg = colorfield[x][y])
+                square.grid(row = x, column = y)
+                #square.bind("<Button-1>", on_click)
+
+        gridFrame.pack()
         bottom=Button(gameFrame,text="Main Menu",command=lambda:self.display(window, gameFrame))
         bottom.pack()
 
     def getInstructions(self, window):
         screenWidth= window.winfo_screenwidth()               
         screenHeight= window.winfo_screenheight()  
-        gameFrame=Frame(window,width=screenWidth, height=screenHeight)
+        instructFrame=Frame(window,width=screenWidth, height=screenHeight)
 
-        top=Label(gameFrame,text="Get good")
+        top=Label(instructFrame,text="Get good")
         top.pack()
-        gameFrame.pack_propagate(0)
+        instructFrame.pack_propagate(0)
         
-        gameFrame.place(anchor=NW)
-        gameFrame.tkraise()
+        instructFrame.place(anchor=NW)
+        instructFrame.tkraise()
 
-        bottom=Button(gameFrame,text="Main Menu",command=lambda:self.display(window, gameFrame))
+        bottom=Button(instructFrame,text="Main Menu",command=lambda:self.display(window, instructFrame))
         bottom.pack()
 
     def setSettings(self, window):
         screenWidth= window.winfo_screenwidth()               
         screenHeight= window.winfo_screenheight()  
-        gameFrame=Frame(window,width=screenWidth, height=screenHeight)
+        setFrame=Frame(window,width=screenWidth, height=screenHeight)
 
-        top=Label(gameFrame,text="Mode is ?")
+        top=Label(setFrame,text="Mode is ?")
         top.pack()
-        gameFrame.pack_propagate(0)
-        
-        gameFrame.place(anchor=NW)
-        gameFrame.tkraise()
+        setFrame.pack_propagate(0)
 
-        bottom=Button(gameFrame,text="Main Menu",command=lambda:self.display(window, gameFrame))
+        setFrame.place(anchor=NW)
+        setFrame.tkraise()
+
+        bottom=Button(setFrame,text="Main Menu",command=lambda:self.display(window, setFrame))
         bottom.pack()
 
 
